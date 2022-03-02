0fd34732df81ed1c5b61915985b54ab4586e23ec
Percell Gaskins <Pgaskins@users.noreply.github.com>
2022-03-02 21:26:32 +0000

    mixed list

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#create a list containing three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#display the my_list list first item[0]
+print("The first item in the list (IP):" + my_list[0] )
+
+#display the second item in the my_list item[1]
+print("The second item in the list (port): " + str(my_list[1]) )
+
+#display the third item in the my_list item[2]
+print("The last item in the list (state): " + my_list[2] )
+
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 1 - add up the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+# example 2 - use the comma separator
+print("IP addresses:", iplist[3], ", and", iplist[4])
+
+
