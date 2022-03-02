9ad36ef00a037af9d5845aedae25c77d4599e430
Percell Gaskins <Pgaskins@users.noreply.github.com>
2022-03-02 21:09:29 +0000

    learning about input function

---
+++ no_shebang.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+x=5*5
+print(x)
+num =[45,44,34,56,77,32,72,3,2] 
+print("What is your quest?")
+#pause and collect input(wait for enter)
+quest = input()
+quest = input("What is your Quest?")
+
