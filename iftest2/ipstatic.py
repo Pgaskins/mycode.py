2c700f0f7042ea20a8be8f27babbd86f34cfaa51
Percell Gaskins <Pgaskins@users.noreply.github.com>
2022-03-03 20:47:08 +0000

    if

--- iftest2/ipstatic.py
+++ iftest2/ipstatic.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Alta3 Research | RZFeeser
-   Conditionals - strings test true"""
+ipchk = input("Apply an IP address: ") # this line now prompts the user for input
 
-ipchk = input("Apply IP address: ")
-
-# a string tests as True
-if ipchk:
-   print("Looks like the IP address was set: " + ipchk)
+# if user set IP of gateway
+if ipchk == "192.168.70.1":
+   print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
+elif ipchk: # if any data is provided, this will test true
+   print("Looks like the IP address was set: " + ipchk) # indented under if
+else: # if data is NOT provided
+   print("You did not provide input.") # indented under else
 
